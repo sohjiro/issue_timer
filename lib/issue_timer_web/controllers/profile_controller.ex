ca43cf5ee2d68d84c123daad8d3e243306cfd204
Sohjiro <felipe@makingdevs.com>
2017-08-08 11:46:42 -0500

    #3 fetching user's repo and passing to the view

--- lib/issue_timer_web/controllers/profile_controller.ex
+++ lib/issue_timer_web/controllers/profile_controller.ex
@@ -3,9 +3,9 @@ defmodule IssueTimerWeb.ProfileController do
 
   def profile(conn, _params) do
     current_user = get_session(conn, :current_user)
-    IssueTimer.Core.Github.Repo.list_mine(current_user)
+    repos = IssueTimer.Core.Github.Repo.list_mine(current_user)
 
-    render conn, "profile.html", current_user: current_user
+    render conn, "profile.html", current_user: current_user, repos: repos
   end
 
 end
