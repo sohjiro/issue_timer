6080cf35a816499b0bd224f798d285365cb3c7a6
sohjiro <felipe@makingdevs.com>
2017-08-07 23:37:14 -0500

    #· rendering current_user and calling list_mine WIP

--- lib/issue_timer_web/controllers/profile_controller.ex
+++ lib/issue_timer_web/controllers/profile_controller.ex
@@ -2,13 +2,10 @@ defmodule IssueTimerWeb.ProfileController do
   use IssueTimerWeb, :controller
 
   def profile(conn, _params) do
-
     current_user = get_session(conn, :current_user)
-    # Tentacat.Client.new(%{access_token: current_user.token})
-    # |> Tentacat.Repositories.list_mine
-    # |> IO.inspect
+    IssueTimer.Core.Github.Repo.list_mine(current_user)
 
-    render conn, "profile.html", current_user: get_session(conn, :current_user)
+    render conn, "profile.html", current_user: current_user
   end
 
 end
