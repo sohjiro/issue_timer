ec591deeb8540d7bf038520a7e5521009940e7ad
Sohjiro <felipe@makingdevs.com>
2017-08-09 11:00:05 -0500

    fixing structure for url

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,8 @@
 use Mix.Config
+
 config :issue_timer, IssueTimerWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: {:system, "HOST", port: 80],
+  url: [host: {:system, "HOST"}, port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
